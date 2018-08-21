3076fab87b9df86ad2b2cc8bb668924ad686fbad
Igor Gitman <ivypawn@gmail.com>
2018-08-21 13:29:17 +0700

    Fix bug with configuration disappering on hover

--- elements/static/js/paper.js
+++ elements/static/js/paper.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     var timer;
-    $('.result-value').mouseenter(function() {
+    $('.value-container').mouseenter(function() {
         var config_id = this.id.slice(6);
         timer = setTimeout(function(){
             $('#config-' + config_id).show();
         }, 500);
     }).mouseleave(function() {
         var config_id = this.id.slice(6);
-        timer = setTimeout(function(){
+        timer = setTimeout(function () {
             $('#config-' + config_id).hide();
         }, 500);
     });
