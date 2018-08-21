a39c76ce0081c389cd8dd23d218153821c73c77e
Igor Gitman <ivypawn@gmail.com>
2018-08-21 13:14:15 +0700

    Add internal links to paper description

---
+++ elements/templatetags/internal_links.py
@@ -0,0 +1,34 @@
+from django import template
+from django.urls import reverse
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+from django.urls import NoReverseMatch
+
+import re
+
+
+register = template.Library()
+
+
+def localurl(match):
+  string = match.group()
+  # Strip off the {{ and }}
+  string = string[2:-2]
+  # Separate the link type and the id
+  link_text, link = string.split("|")
+  link_type, link_id = link.split(":")
+  link_url = reverse('elements:'+link_type, args=(link_id,))
+  return '<a href={}>{}</a>'.format(link_url, link_text)
+
+
+@register.filter
+def internal_links(value):
+  """Filter for internal links in the format {{<type>:<id>}}, i.e.{{paper:1}}.
+  """
+  value = conditional_escape(value)
+  try:
+    pattern = '{{.+\|\S+:\S+}}'
+    p = re.compile(pattern)
+    return mark_safe(p.sub(localurl, value))
+  except NoReverseMatch as e:
+    return '[[Link error: {}]]'.format(e.args[0])
